495c43dcba1e82f5df9ca3c769ccacf7a549db2c
mkay0961 <mkay0961@gmail.com>
2019-05-23 14:46:13 -0400

    added back auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
   def show
-    # token = request.headers["Authentication"].split(' ')[1]
-    # payload = decode(token)
-    # @user = User.find(payload["user_id"])
+    token = request.headers["Authentication"].split(' ')[1]
+    payload = decode(token)
+    @user = User.find(payload["user_id"])
     @user = User.find(params["id"])
     if @user
       render json: @user.generateUserObj()
